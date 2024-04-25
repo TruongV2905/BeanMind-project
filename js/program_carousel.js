90156215b9788ba97ae405076176d29ecb5424a0
TruongV2905 <truong0345218921@gmail.com>
2024-04-25 15:57:46 +0700

    add color and background

--- js/program_carousel.js
+++ js/program_carousel.js
@@ -1,4 +1,4 @@
-$('.lessons__wrap').slick({
+$(".lessons__wrap").slick({
   dots: false,
   prevArrow: '<button type="button" class="slick-pre"><i class="fa fa-arrow-left"></button>',
   nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></button>',
@@ -6,24 +6,23 @@ $('.lessons__wrap').slick({
   speed: 500,
   cssEase: "ease",
   autoplay: false,
-  draggable: true,
   centerPadding: "0px",
-  centerMode: true,
-  slidesToScroll:1,
+  centerMode: false,
+  slidesToScroll: 1,
   slidesToShow: 4,
   zIndex: 1,
   // adaptiveHeight: true,
   pauseOnHover: true,
   responsive: [
     {
-      breakpoint: 1000,
+      breakpoint: 1023,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
         centerPadding: "0px",
         infinite: true,
-        dots: false
-      }
+        dots: false,
+      },
     },
     {
       breakpoint: 767,
@@ -32,12 +31,21 @@ $('.lessons__wrap').slick({
         slidesToScroll: 1,
         centerPadding: "0px",
         infinite: true,
-        dots: false
-      }
+        dots: false,
+      },
     },
-  
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "0px",
+        infinite: true,
+        dots: false,
+      },
+    },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
-  ]
+  ],
 });
